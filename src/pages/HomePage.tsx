f2fb2badcd98351cc5725b73ee22dae03f4b1d27
Brandon Zhang <bzuruce@gmail.com>
2022-11-25 10:16:10 -0800

    bugs: fix zustand store

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,32 +4,43 @@ import './HomePage.css'
 import dayjs from 'dayjs'
 import { useNewTabStore } from '../store';
 
-let initDate = dayjs(new Date());
+let djs = dayjs(new Date());
 
 export function HomePage() {
-  let d = new Date
-  let [time, setTime] = useState<string>(initDate.format("h:mm"))
-  let [ampm, setAmPm] = useState<string>(initDate.format("A"))
-  let [date, setDate] = useState<string>()
-
-  const {userTodo} = useNewTabStore();
-  let [todo, setTodo] = useState<string>('Nothing')
-  let [quickLinkName, setQuickLinkName] = useState<string>('Google')
-  let [quickLinkURL, setQuickLinkURL] = useState<string>('https://google.com')
-  let [searchBar, setSearchBar] = useState<string>('Boz')
-
+  const {userName, userTodo, urlName, urlLink} = useNewTabStore();
+  let [time, setTime] = useState<string>(djs.format("h:mm"))
+  let [ampm, setAmPm] = useState<string>(djs.format("A"))
+  let greeting = "Hi, " + userName
+  if (time >= "7:00" && time <= "11:59" && ampm == "AM"){
+    greeting = "Good Morning, " + userName
+  }
+  if (time >= "12:00" && time <= "3:00" && ampm == "PM"){
+    greeting = "Good Afternoon, " + userName
+  }
+  if (time >= "3:00" && time <= "11:59" && ampm == "PM" ){
+    greeting = "Good Evening, " + userName
+  }
+  if (time >= "12:00" && time <= "6:59" && ampm == "AM"){
+    greeting = "Good Night, " + userName
+  }
+  let dateAndGreeting = djs.format("dddd, MMMM DD, YYYY") + " - " + greeting
+  let [date, setDate] = useState<string>(djs.format("dddd, MMMM DD, YYYY"))
   return (
     <div className="App">
       <form action="https://www.google.com/search" method="get" target="_blank" id="search-form">
       <input name="q" type="text" placeholder="Google Search"></input>
       </form>
-      <div className="mediumtitle">{date}</div>
+      <div className="mediumtitle">{dateAndGreeting}</div>
       <div className="bigtitle">{time}</div> <div className="mediumtitle">{ampm}</div><br></br>
       <div className="lessmediumtitle">What you need to do today: </div>
       <div className="lessbigtitle">{userTodo}</div>
       <br></br>
-      <button className="quick-link" onClick={() => window.location.href=quickLinkURL}>{quickLinkName}</button><br></br><br></br>
-      <button className="quick-link" onClick={() => window.location.href="/settings"}>Settings</button>
+      <button className="quick-link" onClick={() => window.location.href = urlLink }>URL: {urlName}</button><br></br><br></br>
+      <button className="quick-link" onClick={() => location.href="/settings"}>Settings</button>
     </div>
   )
+  let timeSinceNewMinute = djs.format("s")
+  let timeUntilNewMinute = 60-parseInt(timeSinceNewMinute)
+  let timeoutUntilNewMinute = 1000*timeUntilNewMinute
+  setTimeout(() => {location.reload()}, timeoutUntilNewMinute)
 }
--- src/pages/SetupPage.tsx
+++ src/pages/SetupPage.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from '@tanstack/react-location'
 import { string } from 'prop-types'
 import { useEffect, useRef, useState } from 'react'
 import { useNewTabStore } from '../store'
@@ -5,22 +6,35 @@ import reactLogo from './assets/react.svg'
 import './SettingsPage.css'
 
 export function SetupPage() {
+  const nameRef = useRef<HTMLInputElement>(null);
   const todoRef = useRef<HTMLInputElement>(null);
+  const qlNameRef = useRef<HTMLInputElement>(null);
+  const qlURLRef = useRef<HTMLInputElement>(null);
+  const {userName, setUserName} = useNewTabStore();
   const {userTodo, setUserTodo} = useNewTabStore();
-
+  const {urlName, setUrlName} = useNewTabStore();
+  const {urlLink, setUrlLink} = useNewTabStore();
+  const navigate = useNavigate()
   const onUpdate = () => {
-    const value = todoRef.current?.value || ''
-    setUserTodo(value);
-    location.href = "/"
+    const namevalue = nameRef.current?.value || 'User'
+    setUserName(namevalue);
+    const todovalue = todoRef.current?.value || "You're good to go!"
+    setUserTodo(todovalue);
+    const qlnamevalue = qlNameRef.current?.value || "You don't have a quick link! Click here to set it up"
+    setUrlName(qlnamevalue);
+    console.log('setUrlName', qlnamevalue)
+    const qlURLvalue = qlURLRef.current?.value || '/setup'
+    setUrlLink(qlURLvalue);
+    navigate({ to: '/', replace: true })
   }
-  
-  useEffect( () => setUserTodo(userTodo), []);
-
   return (
     <div className="App">
       <div className="lessbigtitle">Setup</div>
       <br></br>
-      <input ref={todoRef} id='todo' placeholder='What you need to do' /*value = {userTodo}*/></input>
+      <input ref={nameRef} id='todo' placeholder='What is your name?'></input>
+      <input ref={todoRef} id='todo' placeholder='What do you need to do'></input>
+      <input ref={qlNameRef} id='todo' placeholder='What is the name of the website you visit most'></input>
+      <input ref={qlURLRef} id='todo' placeholder='What is the URL of the website you visit most'></input>
       <button className="quick-link" onClick={onUpdate}>Submit and Proceed</button>
     </div>
   )
